229f4b81b0acb65d8e239092c5ef9a1151f90ce2
Kyle Taylor <kylepattontaylor@gmail.com>
2024-02-28 22:29:38 -0500

    Finalized movies-app

--- 14.1 jQuery/movies-app/script.js
+++ 14.1 jQuery/movies-app/script.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
     $('#new-movie').on("click", function() {
-        let title = $('#title').val();
-        let rating = $('#rating').val();  
-        $('#entered-movies').append('<div>' + title + rating + '</div>')
+        let $title = $('#title').val();
+        let $rating = $('#rating').val();  
+        let $newMovie = ('<div class="movie"><h2>' + $title + '</h2>'+ '<h3>' + $rating + '</h3><button class="remove">Remove</button></div>');
+        $('#entered-movies').append($newMovie);
+    });
+
+    $(document).on('click', '.remove', function () {
+        $(this).parent('.movie').remove();
     });
 });
