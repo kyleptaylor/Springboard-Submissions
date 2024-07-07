fc6eb08078fed6c8c76c49d9b15889aaced38ad8
Kyle Taylor <kylepattontaylor@gmail.com>
2024-07-07 13:33:29 -0600

    Completed 17

--- 19.2 Python Data Structures/17_mode/mode.py
+++ 19.2 Python Data Structures/17_mode/mode.py
@@ -11,3 +11,8 @@ def mode(nums):
         >>> mode([2, 2, 3, 3, 2])
         2
     """
+    num_count = {}
+    for num in nums:
+        num_count[num] = num_count.get(num, 0) + 1
+    
+    return max(num_count, key=num_count.get)
