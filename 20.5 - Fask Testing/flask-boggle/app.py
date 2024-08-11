4ece3cc9a6563a9b59d682c662e5d310e17c4b00
Kyle Taylor <kylepattontaylor@gmail.com>
2024-08-10 20:42:32 -0400

    Added additional files and directories

--- 20.5 - Fask Testing/flask-boggle/app.py
+++ 20.5 - Fask Testing/flask-boggle/app.py
@@ -1,3 +1,19 @@
+# Import Tools
+from flask import Flask, request, render_template, redirect, flash, jsonify, url_for, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+# Import Files
 from boggle import Boggle
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "bogglemaster"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
 boggle_game = Boggle()
+
+@app.route('/boggle')
+def render_board():
+    return render_template("boggle.html")
+    
