76493c79dbe5f7a9333bca06379003456dd8b821
Kyle Taylor <kylepattontaylor@gmail.com>
2024-08-13 22:18:41 -0400

    Completed countZeros

--- 20.6 - Divide and Conquer/count-zeroes.js
+++ 20.6 - Divide and Conquer/count-zeroes.js
@@ -1,5 +1,20 @@
-function countZeroes() {
-  
+function countZeroes(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor(left + (right - left) / 2);
+
+    if (arr[mid] === 0 && (mid === 0 || arr[mid - 1] === 1)) {
+      return arr.length - mid;
+    } else if (arr[mid] === 0) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return 0;
 }
 
-module.exports = countZeroes
+module.exports = countZeroes;
