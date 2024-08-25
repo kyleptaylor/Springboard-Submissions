dabb2052743e57f613d279ff483aa5e8145891a2
Kyle Taylor <kylepattontaylor@gmail.com>
2024-08-24 20:43:14 -0400

    Added 23.2

---
+++ 23.2 - SQL Querying/sql-querying/queries_products.sql
@@ -0,0 +1,29 @@
+-- Comments in SQL Start with dash-dash --
+
+-- 1. Add a product to the table with the name of “chair”, price of 44.00, and can_be_returned of false.
+
+-- 2. Add a product to the table with the name of “stool”, price of 25.99, and can_be_returned of true.
+
+-- 3. Add a product to the table with the name of “table”, price of 124.00, and can_be_returned of false.
+
+-- 4. Display all of the rows and columns in the table.
+
+-- 5. Display all of the names of the products.
+
+-- 6. Display all of the names and prices of the products.
+
+-- 7. Add a new product - make up whatever you would like!
+
+-- 8. Display only the products that ***can_be_returned***
+
+-- 9. Display only the products that have a price less than 44.00.
+
+-- 10. Display only the products that have a price in between 22.50 and 99.99.
+
+-- 11. There’s a sale going on: Everything is $20 off! Update the database accordingly.
+
+-- 12. Because of the sale, everything that costs less than $25 has sold out. Remove all products whose price meets this criteria.
+
+-- 13. And now the sale is over. For the remaining products, increase their price by $20.
+
+-- 14. There is a new company policy: everything is returnable. Update the database accordingly.
