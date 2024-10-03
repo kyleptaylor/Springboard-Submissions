4f52e7664b4e09dcc379900fadfbda6981c7c070
Kyle Taylor <kylepattontaylor@gmail.com>
2024-10-02 21:49:26 -0400

    Completed skeleton

--- 24.1 - SQLAlchemy Intro/flask-blogly/app.py
+++ 24.1 - SQLAlchemy Intro/flask-blogly/app.py
@@ -1,13 +1,26 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "chickenPotPie"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+# Added for app context
+app.app_context().push() 
+
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
 
+@app.route('/')
+def show_users():
+    """ List all users from db """
+    users = User.query.all()
+    return render_template('users.html', users = users)
--- 24.1 - SQLAlchemy Intro/flask-blogly/models.py
+++ 24.1 - SQLAlchemy Intro/flask-blogly/models.py
@@ -1,2 +1,35 @@
 """Models for Blogly."""
 
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """ User """
+    __tablename__ = "users"
+
+    def __repr__(self):
+        u = self
+        return f"<User(id={u.id}, first_name={u.first_name}, last_name={u.last_name})>"
+
+    @classmethod
+    def get_all_users(cls):
+        return cls.query.filter(User).all()
+
+    id = db.Column(db.Integer, 
+                   primary_key=True, 
+                   autoincrement=True)
+    
+    first_name = db.Column(db.String(30), 
+                           nullable=False)
+    
+    last_name = db.Column(db.String(30), 
+                          nullable=False)
+    
+    image_url = db.Column(db.String, 
+                          nullable=True,
+                          default="https://lastfm.freetls.fastly.net/i/u/770x0/5342164d2994e7f3e0542905546defa5.jpg#5342164d2994e7f3e0542905546defa5")
